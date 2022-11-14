ae862ec272aaf5b5834f20bbdcb01b3dd94e26b6
TKash89 <tyler.kasheta@outlook.com>
2022-11-14 10:13:30 -0800

    first task done with value error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react"
 import "./App.css"
-
+import Square from "./components/Square"
 const App = () => {
   const [board, setBoard] = useState([
+   
     "?",
     "?",
     "?",
@@ -13,10 +14,14 @@ const App = () => {
     "?",
     "?"
   ])
-
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <div className="gameboard">
+      {board.map(value, index => {
+       return <Square value = {value} key={index} />
+      })}
+      </div>
     </>
   )
 }
